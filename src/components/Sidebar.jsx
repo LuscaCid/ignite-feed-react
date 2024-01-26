ad711ce4e65f7747c09d1a1e11a6cc56d5d582eb
Lucas Felipe Lima Cid <lucasfelipaaa@gmail.com>
2024-01-26 18:09:15 -0300

    react icons addicted

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import styles from './sidebar.module.css'
+import { FaPencilAlt } from "react-icons/fa";
+
 export const Sidebar = () => {
     const [imgUrl, setImgUrl] = useState('https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=50&w=502&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
 
@@ -25,6 +27,7 @@ export const Sidebar = () => {
             className={styles.footer}
             >
                 <a href="#">
+                    <FaPencilAlt />
                     editar seu perfil
                 </a>
             </footer>
